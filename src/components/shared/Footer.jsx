3fae9849dd3cbfaaaa9f2d3aed37aa0198611f73
HeriEspinosa <espinosa_reyes@hotmail.com>
2023-04-05 12:05:20 -0400

    details

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -7,10 +7,11 @@ const Footer = () => {
             <section className='footer__info'>
                 <section className="info__gratitude flex">
                     <h3 className="info__gratitude-title">Sample</h3>
+                    <a>Thanks</a>
                 </section>
                 <section className="info__code">
                     <h3 className="info__code-title">GitHub</h3>
-                    <a className="letter_Neon" href="https://github.com/HeriEspinosa/repo_react_entregable6.git" target='blank'><p>Hecha un Vistazo a mi Codigo <span>Aqui</span></p></a>
+                    <a className="letter_Neon" href="https://github.com/HeriEspinosa/repo_react_entregable6.git" target='blank'><p>Mi Codigo <span>Aqui</span></p></a>
                 </section>
                 <section className="info__redes">
                     <h3 className="footer__information-title">Social</h3>
