3a94b5eb29ffae4917ff8acf088b4cbf5efa6848
HeriEspinosa <espinosa_reyes@hotmail.com>
2023-04-05 01:42:11 -0400

    purchase

--- src/pages/PurchasesPage.jsx
+++ src/pages/PurchasesPage.jsx
@@ -16,19 +16,53 @@ const PurchasesPage = () => {
     }, [])
 
     return (
-        <div className='purchasesPage flex'>
-            <div className='purchasesPage__item'>
-                {
-                    purchases?.map(purchase => (
-                        <PurchaseCard
-                            key={purchase.id}
-                            purchase={purchase}
-                        />
-                    ))
-                }
+        <div className='purchasesPage'>
+            <div className="purchasesPage__left flex">
+            <div className="">
+                    <img src="/thanksbuy.gif" alt="Amazon" />
+                </div>
+                <h3>Nuestras tiendas</h3>
+                <div className="purchasesPage__left-tienda">
+                    <h5>Amazon</h5>
+                    <img src="/amazon.png" alt="Amazon" />
+                </div>
+                <div className="purchasesPage__left-tienda">
+                    <h5>Ebay</h5>
+                    <img src="/ebay.png" alt="ebay" />
+                </div>
+                <div className="purchasesPage__left-tienda">
+                    <h5>Bestbuy</h5>
+                    <img src="/bestbuy.png" alt="bestbuy" />
+                </div>
+                <div className="purchasesPage__left-tienda">
+                    <h5>Myus</h5>
+                    <img src="/myus.png" alt="myus" />
+                </div>
+                <div className="purchasesPage__left-tienda">
+                    <h5>Walmart</h5>
+                    <img src="/walmart.png" alt="walmart" />
+                </div>
             </div>
-            <div className="purchasesPage__slice flex">
-                <img src={'/public/thanksbuy.gif'} alt="" />
+            <div className="purchasesPage__right flex">
+                <div className="purchasesPage__searh">
+                    <ul className='purchasesPage__header-ul'>
+                        <li>All</li>
+                        <li>TV</li>
+                        <li>Phone</li>
+                        <li>Laptop</li>
+                    </ul>
+                </div>
+                
+                <div className='purchasesPage__item flex'>
+                    {
+                        purchases?.map(purchase => (
+                            <PurchaseCard
+                                key={purchase.id}
+                                purchase={purchase}
+                            />
+                        ))
+                    }
+                </div>
             </div>
         </div>
 
